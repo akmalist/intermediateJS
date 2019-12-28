bc4e8ae8890a6da07cc3716789aae4a16417c4de
Aki <akmalist@gmail.com>
2019-12-28 10:25:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -73,6 +73,24 @@
 // }
 
 
-sumAll([1, 4])
+// sumAll([1, 4])
+
+
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+// You have to use the arguments object.
+
+function destroyer(arr) {
+  const  result  = Array.prototype.slice.call(arguments);
+  const [first, ...rest] = result;
+
+  const fileList = first.filter(function(event) {
+  return rest.indexOf(event) ===-1;
+ })
+
+ return fileList;
+}
+  
+destroyer  ([1, 2, 3, 5, 1, 2, 3], 2, 3)
+
   
  
